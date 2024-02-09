342b6fc3fb6ae7cd653d94ef3a3314a7c4db4a89
HanniSalman <hannisalman2510@outlook.com>
2024-02-09 21:01:48 +0100

    .

--- week02/threads/exercise2/Main.java
+++ week02/threads/exercise2/Main.java
@@ -1,4 +1,17 @@
 package week02.threads.exercise2;
 
 public class Main {
+    private static class Counter {
+        private int count = 0;
+
+        public synchronized void increment() {
+            count++;
+        }
+
+
+        public int getCount() {
+            return count;
+        }
+    }
 }
+
