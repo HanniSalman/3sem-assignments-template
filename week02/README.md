d0418b846b48337547f5561f5d2a88fc3892e59e
HanniSalman <hannisalman2510@outlook.com>
2024-02-09 22:16:48 +0100

    .

--- week02/README.md
+++ week02/README.md
@@ -3,8 +3,8 @@
 ## Overview
 
 - Hanni Salman
-- Assignments from day 1: task 1 and 2 are included. From day 2 threads: Exercise 1,2,3,4,6 are included. 
-- All are completed.
+- Assignments from day 1: task 2 is included. From day 2 threads: Exercise 1,2,3,4, are included. 
+- Assignments from day 1 task 1 and from day 2 exercise 6 aren't included.
 
 ## Friday presentation
 - From day 2 threads: Exercise 1, is the exercise I'm going to show on friday. 
--- week02/threads/exercise4/Main.java
+++ week02/threads/exercise4/Main.java
@@ -1,4 +1,38 @@
 package week02.threads.exercise4;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+    public static void main(String[] args) {
+        int numCores = Runtime.getRuntime().availableProcessors();
+
+        ExecutorService executor = Executors.newFixedThreadPool(numCores);
+
+        for (int i = 0; i < numCores; i++) {
+            executor.execute(new CPUBoundTask());
+        }
+
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All tasks completed.");
+    }
+
+    static class CPUBoundTask implements Runnable {
+        @Override
+        public void run() {
+            long result = 0;
+            for (int i = 0; i < Math.pow(10, 7); i++) {
+                result += i;
+            }
+            System.out.println("Task completed with result: " + result);
+        }
+    }
 }
